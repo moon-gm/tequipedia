b4d185b64ddc8b166a66e32353bed94d2e5a255a
= <=>
2021-09-18 20:04:12 +0900

    masterの変更を更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,16 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - tequipedia',
-    title: 'tequipedia',
+    title: 'Tequipedia',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'テキーラ検索サイト' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:url', property: 'og:url', content: 'https://tequipedia.vercel.app/' },
+      { hid: 'og:title', property: 'og:title', content: 'Tequipedia' },
+      { hid: 'og:description', property: 'og:description', content: '好みのテキーラを検索したりできるサイト' },
+      { hid: 'og:image', property: 'og:image', content: '/favicon.ico' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
